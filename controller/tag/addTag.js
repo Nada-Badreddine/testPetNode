7bf4d414bafcb75c32ade1d38d762cd318724f67
Nada-Badreddine <badrenadaa@gmail.com>
2022-07-16 19:01:57 +0100

    test1

--- controller/tag/addTag.js
+++ controller/tag/addTag.js
@@ -2,7 +2,7 @@ import Tag from '../../models/tag'
 
 const  addTag = async (req, res) => {
     try {
-        console.log("test 5")
+        console.log("test 6")
         const tag = await Tag.create(req.body);
         return res.json({ message: "ok", status: 200, result: tag }).status(200);
     }
